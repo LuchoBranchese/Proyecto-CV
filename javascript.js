8a2c3b68b944ec0018ff3236337d207c3571f415
LuchoBranchese <123122409+LuchoBranchese@users.noreply.github.com>
2023-02-23 18:50:21 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+document.getElementById('Info').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'FF0000'
+});
+
+document.getElementById('Educación').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'FF0000'
+});
+
+document.getElementById('Habilidades').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'FF0000'
+});
+
+document.getElementById('Experiencia').addEventListener('click', function() {
+    document.body.style.backgroundColor = 'FF0000'
+});
