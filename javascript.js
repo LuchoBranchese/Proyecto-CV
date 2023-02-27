ae6a68334a3b0f55dfa9144aed8ffae5b3071978
Luciano <luchobranchese@gmail.com>
2023-02-27 20:04:02 -0300

    Crea Proyecto CV

---
+++ javascript.js
@@ -0,0 +1,9 @@
+function abrirMapa(){
+window.open("https://www.google.com.ar/maps/place/Bv.+25+de+Mayo+1278,+Devoto,+C%C3%B3rdoba/@-31.4065928,-62.3065279,17z/data=!3m1!4b1!4m5!3m4!1s0x95cb322dce6e5ec5:0x9f40b27fff00906c!8m2!3d-31.4065974!4d-62.3043392");
+
+}
+
+
+document.getElementById('direccion').addEventListener('click', function() {
+    window.open()
+});
